1ca0b903cc0865b4633015e0a72001e1e1d84c9e
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-06-29 16:39:41 -0400

    Map

--- web/main.dart
+++ web/main.dart
@@ -2,19 +2,14 @@ import 'dart:html';
 
 void main() {
 querySelector('#output').text = 'Your Dart app is running.'; 
-// Varibale Decleartion 
-List fruits = ['Apple', 'Pear', 'Banana'];
-List count = [1,2,3,4,5];
 
-List vegetables = new List();
-vegetables.add("peas");
-vegetables.add("Tomoto");
+Map nameAndAges = {
+  'Dipesh' : 25,
+  'Sanket' : 26,
+  'Parth' : 27
+};
 
-List fixedFizeList = new List(4);
-fixedFizeList[0] = "ABC";
-
-print(fruits);
-print(count);
-print(vegetables);
-print(fixedFizeList);
+nameAndAges['Ishrat'] = 102;
+print(nameAndAges);
+print(nameAndAges['Sanket']);
 }
