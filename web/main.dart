48b731c189649318893cbee0d5afe93b3be2c1fc
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-06-29 16:24:41 -0400

    Variables - String

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,26 @@
 import 'dart:html';
 
 void main() {
+  // Varibale Decleartion 
+
+  // String concatenation
+  String firstName = "Dipesh";
+  String name = "Dipesh" " B" " Goswami";
+
+  // Triple Quote
+  var lastname = """ 
+  Test 1
+  Test 2
+  Test 3
+  """;
+
+  // string interpolation 
+  String greeting = "Hello ${firstName}";
+  print(greeting);
+ 
   querySelector('#output').text = 'Your Dart app is running.';
+
+  // Print the result in Console 
+  print(name);
+  print(lastname);
 }
